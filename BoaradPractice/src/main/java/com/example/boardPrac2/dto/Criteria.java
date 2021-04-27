782f4b73db8c98feba5e3243446b37fbd6b512af
hanry19 <63430211+hanry19@users.noreply.github.com>
2021-04-28 01:34:56 +0900

    re_upLoad

--- BoaradPractice/src/main/java/com/example/boardPrac2/dto/Criteria.java
+++ BoaradPractice/src/main/java/com/example/boardPrac2/dto/Criteria.java
@@ -11,8 +11,14 @@ import lombok.extern.slf4j.Slf4j;
 @ToString
 public class Criteria {
 
-    private int pageNum;            // 보여줄 페이지 번호
+    private int pageNum;   // 보여줄 페이지 번호 = 현재 페이지
     private int amount;   // 페이지당 보여줄 게시글의 개수
+
+
+    // 동적 검색을 위한 멤버 변수
+    private String type;        // t, tx, tcw. cw. 이런 식으로 하려고 함 ==> 배열 필요
+    private String keyword;
+
 
     public Criteria() {
         this(1, 10);
@@ -22,4 +28,13 @@ public class Criteria {
         this.pageNum = pageNum;
         this.amount = amount;
     }
+
+    // loop를 돌리 기 위해 배열이 필요하다.
+    public String[] getTypeArr() {
+        // string 배열이 null이 안나오게 하려고 빈 배열을 준다.
+        return  type == null? new String[] {} : type.split("");
+
+
+    }
+
 }
